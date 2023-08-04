39ba92095f270f3db101e54370932974567253db
dovanrin <vanrinrt@gmail.com>
2023-08-04 09:51:59 +0700

    quan ly phim

--- src/services/movieServices.jsx
+++ src/services/movieServices.jsx
@@ -4,4 +4,13 @@ export const movieSer = {
   getAllBanner: () => {
     return https.get("/api/QuanLyPhim/LayDanhSachBanner");
   },
+  getAllMovie: () => {
+    return https.get("/api/QuanLyPhim/LayDanhSachPhim?maNhom=GP09");
+  },
+  delMovie: (maPhim) => {
+    return https.delete(`/api/QuanLyPhim/XoaPhim?MaPhim=${maPhim}`);
+  },
+  addPhim: (formData) => {
+    return https.post(`/api/QuanLyPhim/CapNhatPhimUpload`, formData);
+  },
 };
